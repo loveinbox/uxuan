be98baa85f53ed04f063658bda9f4cfd85082c7c
Hank <dhuhank@foxmail.com>
2016-08-10 22:25:18 +0800

    alert location,config in done

--- conference/www/js/controllers.js
+++ conference/www/js/controllers.js
@@ -27,6 +27,16 @@ angular.module('starter.controllers', ['starter.services'])
                         jsApiList: ['checkJsApi', 'openAddress', 'getLocation']
                     });
                     wx.error(function(res) {});
+                    wx.getLocation({
+                        type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+                        success: function (res) {
+                            $rootScope.latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+                            $rootScope.longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+                            $rootScope.speed = res.speed; // 速度，以米/每秒计
+                            $rootScope.accuracy = res.accuracy; // 位置精度
+                            alert(res.latitude);
+                        }
+                    });
                 })
                 .fail(function(e) {
                     // alert(e);
@@ -56,18 +66,6 @@ angular.module('starter.controllers', ['starter.services'])
         // note: the indexes are 0-based
         $scope.activeIndex = data.activeIndex;
         $scope.previousIndex = data.previousIndex;
-    });
-
-
-    wx.getLocation({
-        type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
-        success: function (res) {
-            $rootScope.latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-            $rootScope.longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
-            $rootScope.speed = res.speed; // 速度，以米/每秒计
-            $rootScope.accuracy = res.accuracy; // 位置精度
-            alert(res.latitude);
-        }
     });
 
 
