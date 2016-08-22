55abc62d1e794ec74c3994b2cd1bd6440bc4cf99
Hank <dhuhank@foxmail.com>
2016-08-22 20:56:30 +0800

    default location 3

--- conference/www/js/services.js
+++ conference/www/js/services.js
@@ -85,6 +85,7 @@ angular.module('starter.services', ['ngResource'])
             wx.getLocation({
                 type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
                 success: function(res) {
+                    console.log('location success', s);
                     $rootScope.latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
                     $rootScope.longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
                     $rootScope.speed = res.speed; // 速度，以米/每秒计
