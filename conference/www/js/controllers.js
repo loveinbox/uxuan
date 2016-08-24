1e60aa23d7d5b7e4323eff5163481857c8a0db50
Hank <dhuhank@foxmail.com>
2016-08-25 00:16:27 +0800

    horiziontal scroll fixed

--- conference/www/js/controllers.js
+++ conference/www/js/controllers.js
@@ -18,28 +18,73 @@ angular.module('starter.controllers', ['starter.services'])
     //     });
 })
 
-.controller('SessionsCtrl', function($scope, $rootScope, NearByEguard, MainPageHot, NearByFruitShops, FruitUxuanRank, Location, ShoppingCart) {
+.controller('SessionsCtrl', function($scope, $rootScope, $timeout, $ionicScrollDelegate, NearByEguard, MainPageHot, NearByFruitShops, FruitUxuanRank, Location, ShoppingCart) {
     // Location.then(function() {
-        console.log('get location');
+    console.log('get location');
 
-        MainPageHot.get({
-            'longitude': $rootScope.longitude || 121.483159,
-            'latitude': $rootScope.latitude || 31.3234
-        }, function(data) {
-            $scope.sessions = data.data;
-        }, function(data) {
-            alert('NO DATA');
-        });
+    MainPageHot.get({
+        'longitude': $rootScope.longitude || 121.483159,
+        'latitude': $rootScope.latitude || 31.3234
+    }, function(data) {
+        $scope.sessions = data.data;
+    }, function(data) {
+        alert('NO DATA');
+    });
 
-        FruitUxuanRank.get({
-            'longitude': $rootScope.longitude || 121.483159,
-            'latitude': $rootScope.latitude || 31.3234,
-        }, function(data) {
-            $scope.goods = data.data;
-        }, function(data) {
-            alert('NO DATA');
-        });
+    FruitUxuanRank.get({
+        'longitude': $rootScope.longitude || 121.483159,
+        'latitude': $rootScope.latitude || 31.3234,
+    }, function(data) {
+        $scope.goods = data.data;
+    }, function(data) {
+        alert('NO DATA');
+    });
     // })
+
+    $timeout(function() {
+        //return false; // <--- comment this to "fix" the problem
+        var sv = $ionicScrollDelegate.$getByHandle('horizontal').getScrollView();
+
+        var container = sv.__container;
+
+        var originaltouchStart = sv.touchStart;
+        var originalmouseDown = sv.mouseDown;
+        var originaltouchMove = sv.touchMove;
+        var originalmouseMove = sv.mouseMove;
+
+        container.removeEventListener('touchstart', sv.touchStart);
+        container.removeEventListener('mousedown', sv.mouseDown);
+        document.removeEventListener('touchmove', sv.touchMove);
+        document.removeEventListener('mousemove', sv.mousemove);
+
+
+        sv.touchStart = function(e) {
+            e.preventDefault = function() {}
+            originaltouchStart.apply(sv, [e]);
+        }
+
+        sv.touchMove = function(e) {
+            e.preventDefault = function() {}
+            originaltouchMove.apply(sv, [e]);
+        }
+
+        sv.mouseDown = function(e) {
+            e.preventDefault = function() {}
+            originalmouseDown.apply(sv, [e]);
+        }
+
+        sv.mouseMove = function(e) {
+            e.preventDefault = function() {}
+            originalmouseMove.apply(sv, [e]);
+        }
+
+        container.addEventListener("touchstart", sv.touchStart, false);
+        container.addEventListener("mousedown", sv.mouseDown, false);
+        document.addEventListener("touchmove", sv.touchMove, false);
+        document.addEventListener("mousemove", sv.mouseMove, false);
+    });
+
+
 })
 
 .controller('SessionCtrl', function($rootScope, $scope, $stateParams, $ionicHistory, $ionicModal, FruitDetail, FruitPicShow, ShoppingCart, $ionicModal) {
@@ -604,7 +649,7 @@ angular.module('starter.controllers', ['starter.services'])
         $scope.$broadcast('scroll.refreshComplete');
     }
 
-    $scope.rePay = function (e, order) {
+    $scope.rePay = function(e, order) {
         e.stopPropagation();
         e.preventDefault();
         PayConfirm.get({
@@ -616,7 +661,7 @@ angular.module('starter.controllers', ['starter.services'])
         });
     }
 
-    $scope.cancel = function (e, order) {
+    $scope.cancel = function(e, order) {
         e.stopPropagation();
         e.preventDefault();
         OrderCancel.get({
@@ -651,7 +696,7 @@ angular.module('starter.controllers', ['starter.services'])
     }
     getOrder();
 
-    $scope.rePay = function (e, order) {
+    $scope.rePay = function(e, order) {
         e.stopPropagation();
         e.preventDefault();
         PayConfirm.get({
@@ -667,3 +712,77 @@ angular.module('starter.controllers', ['starter.services'])
 
 
 ;
+
+(function() {
+  var HorizontalScrollFix = (function() {
+    function HorizontalScrollFix($timeout, $ionicScrollDelegate) {
+      return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+          var mainScrollID = attrs.horizontalScrollFix,
+              scrollID = attrs.delegateHandle;
+          
+          var getEventTouches = function(e) {
+            return e.touches && (e.touches.length ? e.touches : [
+              {
+                pageX: e.pageX,
+                pageY: e.pageY
+              }
+            ]);
+          };
+          
+          var fixHorizontalAndVerticalScroll = function() {
+            var mainScroll, scroll;
+            mainScroll = $ionicScrollDelegate.$getByHandle(mainScrollID).getScrollView();
+            scroll = $ionicScrollDelegate.$getByHandle(scrollID).getScrollView();
+            
+            // patch touchstart
+            scroll.__container.removeEventListener('touchstart', scroll.touchStart);
+            scroll.touchStart = function(e) {
+              var startY;
+              scroll.startCoordinates = ionic.tap.pointerCoord(e);
+              if (ionic.tap.ignoreScrollStart(e)) {
+                return;
+              }
+              scroll.__isDown = true;
+              if (ionic.tap.containsOrIsTextInput(e.target) || e.target.tagName === 'SELECT') {
+                scroll.__hasStarted = false;
+                return;
+              }
+              scroll.__isSelectable = true;
+              scroll.__enableScrollY = true;
+              scroll.__hasStarted = true;
+              scroll.doTouchStart(getEventTouches(e), e.timeStamp);
+              startY = mainScroll.__scrollTop;
+              
+              // below is our changes to this method
+              // e.preventDefault();
+             
+              // lock main scroll if scrolling horizontal
+              $timeout((function() {
+                var animate, yMovement;
+                yMovement = startY - mainScroll.__scrollTop;
+                if (scroll.__isDragging && yMovement < 2.0 && yMovement > -2.0) {
+                  mainScroll.__isTracking = false;
+                  mainScroll.doTouchEnd();
+                  animate = false;
+                  return mainScroll.scrollTo(0, startY, animate);
+                } else {
+                  return scroll.doTouchEnd();
+                }
+              }), 100);
+            };
+            scroll.__container.addEventListener('touchstart', scroll.touchStart);
+          };
+          $timeout(function() { fixHorizontalAndVerticalScroll(); });          
+        }
+      };
+    }
+
+    return HorizontalScrollFix;
+
+  })();
+
+  angular.module('starter').directive('horizontalScrollFix', ['$timeout', '$ionicScrollDelegate', HorizontalScrollFix]);
+
+}).call(this);
